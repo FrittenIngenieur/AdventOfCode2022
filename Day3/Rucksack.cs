d79ab0806de013edf1d41c4e31a4830795707d01
FrittenIngenieur <contact@fritteningenieur.de>
2022-12-03 21:23:03 +0100

    Completed Day 3 Part 1

---
+++ Day3/Rucksack.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode2022.Day3;
+
+public class Rucksack
+{
+    public string Compartment1 { get; set; }
+    public string Compartment2 { get; set; }
+    public string Letter { get; set; }
+
+    public Rucksack(string rucksackItems)
+    {
+        Compartment1 = rucksackItems.Substring(0, (int)(rucksackItems.Length/2));
+        Compartment2 = rucksackItems.Substring((int)(rucksackItems.Length/2), (int)(rucksackItems.Length/2));
+
+        var _compartment1Array = Compartment1.ToCharArray();
+        var _compartment2Array = Compartment2.ToCharArray();
+
+        var sameLetterArray = _compartment1Array.Intersect(_compartment2Array);
+
+        foreach (var rucksackItem in sameLetterArray)
+        {
+            Letter = rucksackItem.ToString();
+        }
+    }
+    
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿#define DAY2
+﻿#define DAY3
 
 using AdventOfCode2022.Day1;
 using AdventOfCode2022.Day2;
+using AdventOfCode2022.Day3;
 
 namespace AdventOfCode2022
 {
@@ -15,21 +16,115 @@ namespace AdventOfCode2022
             Console.WriteLine("Solutions for Advent of Code 2022 by FrittenIngenieur");
             Console.WriteLine("#################################################################################");
             
-            #if DAY1
+#if DAY1
             Console.WriteLine("--- Solution Day 1 ---");
             LogicDay1 logicDay1 = new LogicDay1();
             Console.WriteLine("Elf with most Calories has: " + logicDay1.ReturnElfWithMostCalories());
             Console.WriteLine("Top three Elves with most Calories: " + logicDay1.ReturnTopThreeElvesCalories());
             Console.WriteLine("--- End of Day 1 ---");
             
-            #elif DAY2
+#elif DAY2
             Console.WriteLine("--- Solution Day 2 ---");
             LogicDay2 logicDay2 = new LogicDay2();
             Console.WriteLine("Total score of Rock, Scissor, Paper: " + logicDay2.CalculateWinScore());
             Console.WriteLine("Total score of Rock, Scissor, Paper - Part2: " + logicDay2.CalculateWinScorePart2());
             Console.WriteLine("--- End of Day 2 ---");
             
-            #endif
+#elif DAY3
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("Priority sum of all items: " + logicDay3.CalculateSumOfPriorities());
+            Console.WriteLine("--- End of Day 3 ---");
+            
+#elif DAY4
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY5
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY6
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY7
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY8
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY9
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY10
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY11
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY12
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY13
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY14
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY15
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY16
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY17
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY18
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY19
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY20
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY21
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY22
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY23
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY24
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#elif DAY25
+            Console.WriteLine("--- Solution Day 3 ---");
+            LogicDay3 logicDay3 = new LogicDay3();
+            Console.WriteLine("--- End of Day 3 ---");
+#endif
             
         }
     }
